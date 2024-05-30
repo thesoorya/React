0e4fdedc54652bd135e3dfac500b45dc2c83c1a6
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-30 10:37:15 +0530

    login jsx added

--- 01context/src/App.jsx
+++ 01context/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <div>
-      
+      <Login />
     </div>
   )
 }
---
+++ 01context/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2>Login</h2>
+        <p>Username: </p>
+        <input type="text" placeholder='username' />
+        <p>Password: </p>
+        <input type="text" placeholder='password' />
+    </div>
+  )
+}
+
+export default Login
